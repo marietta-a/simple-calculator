1a92f3d796b6ea8b3201bee957425f3cb29f4fec
akumbom <akumbom5ma@gmail.com>
2022-10-25 15:42:45 +0100

    added btn rounded

--- src/App.js
+++ src/App.js
@@ -21,42 +21,42 @@ function App() {
           Learn React
         </a>
       </header> */}
-        <div className="table w-1/2">
+        <div className="table border-separate border-spacing-2 border border-slate-400 w-1/2">
             <div className="table-row bg-white">
               <div className='table-cell'></div>
             </div>
             <div className="table-row bg-white">
               <div className='table-cell'></div>
             </div>
-            <div className="table-row row">
-              <div className='table-cell text-emerald-900 button'>AC</div>
-              <div className='table-cell button'>%</div>
-              <div className='table-cell button'>/</div>
-              <div className='table-cell button'>Back</div>
+            <div className="table-row row text-white">
+              <div className='table-cell border border-slate-300 rounded-md bg-black'>AC</div>
+              <div className='table-cell border border-slate-300 rounded-md bg-black'>%</div>
+              <div className='table-cell border border-slate-300 rounded-md bg-black'>/</div>
+              <div className='table-cell border border-slate-300 rounded-md bg-black'>Back</div>
             </div>
-            <div className="table-row row">
-              <div className='table-cell button'>7</div>
-              <div className='table-cell button'>8</div>
-              <div className='table-cell button'>9</div>
-              <div className='table-cell button'>*</div>
+            <div className="table-row row text-white">
+              <div className='table-cell border border-slate-300 rounded-md bg-black'>7</div>
+              <div className='table-cell border border-slate-300 rounded-md bg-black'>8</div>
+              <div className='table-cell border border-slate-300 rounded-md bg-black'>9</div>
+              <div className='table-cell border border-slate-300 rounded-md bg-black'>*</div>
             </div>
-            <div className="table-row row">
-              <div className='table-cell button'>4</div>
-              <div className='table-cell button'>5</div>
-              <div className='table-cell button'>6</div>
-              <div className='table-cell button'>-</div>
+            <div className="table-row row text-white">
+              <div className='table-cell border border-slate-300 rounded-md bg-black'>4</div>
+              <div className='table-cell border border-slate-300 rounded-md bg-black'>5</div>
+              <div className='table-cell border border-slate-300 rounded-md bg-black'>6</div>
+              <div className='table-cell border border-slate-300 rounded-md bg-black'>-</div>
             </div>
-            <div className="table-row row">
-              <div className='table-cell button'>1</div>
-              <div className='table-cell button'>2</div>
-              <div className='table-cell button'>3</div>
-              <div className='table-cell button'>+</div>
+            <div className="table-row row text-white">
+              <div className='table-cell border border-slate-300 rounded-md bg-black'>1</div>
+              <div className='table-cell border border-slate-300 rounded-md bg-black'>2</div>
+              <div className='table-cell border border-slate-300 rounded-md bg-black'>3</div>
+              <div className='table-cell border border-slate-300 rounded-md bg-black'>+</div>
             </div>
-            <div className="table-row row">
-              <div className='table-cell button' span='2'>0</div>
-              <div className='table-cell button'></div>
-              <div className='table-cell button'>.</div>
-              <div className='table-cell button'>-</div>
+            <div className="table-row row text-white">
+              <div className='table-cell col-span-2 border border-slate-300 rounded-md bg-black'>0</div>
+              <div></div>
+              <div className='table-cell border border-slate-300 rounded-md bg-black'>.</div>
+              <div className='table-cell border border-slate-300 rounded-md bg-black'>-</div>
             </div>
         </div>
     </div>
